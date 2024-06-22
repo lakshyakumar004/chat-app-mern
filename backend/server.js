d94c91a2eb1201679cfdcacf24199b53be1aec29
lakshyakumar004 <125146037+lakshyakumar004@users.noreply.github.com>
2024-06-22 17:07:37 +0530

    third commit

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ const useRoutes = require('./routes/useRoutes');
 const chatRoutes = require('./routes/chatRoutes');
 const messageRoutes = require('./routes/messageRoutes');
 const { notFound, errorHandler } = require('./middleware/errorMiddleware');
+const path = require("path");
 
 dotenv.config();
 connectDB();
@@ -17,13 +18,31 @@ app.use(cors());
 
 app.use(express.json());
 
-app.get('/',(req,res)=>{
-    res.send("API is running");
-});
+// app.get('/',(req,res)=>{
+//     res.send("API is running");
+// });
 
 app.use('/api/user',useRoutes);
 app.use('/api/chat',chatRoutes);
 app.use('/api/message',messageRoutes);
+
+///--------------------deployment--------------------------
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) =>{
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+});
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+
+//------------------------deployment--------------------------
 
 app.use(notFound);
 app.use(errorHandler);
